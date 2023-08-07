677b21b5883a4e0596fa841fc8888ccad79bc152
aryark <aryark3112@gmail.com>
2023-08-07 15:41:28 +0530

    Serialized Commit

---
+++ BinaryDeSerialization/BinaryDeSerialization/Program.cs
@@ -0,0 +1,32 @@
+﻿using EmployeeLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryDeSerialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream writer = new FileStream("D:\\07August\\Assignment24\\BinarySerialization\\BinarySerialization\\employee.bin", FileMode.Open))
+            {
+                Employee desEmployee = (Employee)formatter.Deserialize(writer);
+
+                Console.WriteLine("Binary Deserialization");
+                Console.WriteLine("Employee Id: " + desEmployee.Id);
+                Console.WriteLine("Employee First Name: " + desEmployee.FirstName);
+                Console.WriteLine("Employee Last Name: " + desEmployee.LastName);
+                Console.WriteLine("Salary: " + desEmployee.Salary);
+            }
+
+            Console.WriteLine("\nBinary Deserialization Complete");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ XMLSerialization/XMLSerialization/Program.cs
@@ -0,0 +1,38 @@
+﻿using EmployeeLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XMLSerialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            Console.WriteLine("Enter employee id: ");
+            employee.Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter employee first name");
+            employee.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter employee last name");
+            employee.LastName = Console.ReadLine();
+            Console.WriteLine("Enter employee salary");
+            employee.Salary = double.Parse(Console.ReadLine());
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Employee));
+            using (TextWriter writer = new StreamWriter("D:\\07August\\Assignment24\\XMLSerialization\\XMLSerialization\\employee.xml"))
+            {
+                serializer.Serialize(writer, employee);
+            }
+
+            Console.WriteLine("XML Serialization Complete");
+
+            Console.ReadKey();
+
+        }
+    }
+}
